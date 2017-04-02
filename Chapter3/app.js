c17a9d703bead4d9c3d53afd9a3e2bc8cc786b25
Bill Moore <bill@nzmoore.com>
2017-04-02 19:33:32 -0400

    Simple middleware added to app.js

--- Chapter3/app.js
+++ Chapter3/app.js
@@ -3,9 +3,14 @@ var http = require("http");
 
 var app = express();
 
-app.use(function (request, response) {
+app.use(function(request,response, next) {
   console.log("In comes a request to " + request.url);
-  response.end("Hi there from Express");
+  next();
+});
+
+app.use(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain" });
+    response.end("Hi there from Express with simple middleware");
 })
 
 http.createServer(app).listen(3000);
