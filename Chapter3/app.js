2ccf30d40dc3229ffad77352abab080d626a83d2
Bill Moore <bill@nzmoore.com>
2017-04-02 22:12:40 -0400

    Added route handling

--- Chapter3/app.js
+++ Chapter3/app.js
@@ -1,16 +1,32 @@
 var express = require("express");
 var http = require("http");
 var path = require("path");
+var reqLogger = require("morgan");
 
 var app = express();
+
+app.use(reqLogger("short"));
 
 var publicPath = path.resolve(__dirname, "public");
 console.log(publicPath);
 app.use(express.static(publicPath));
 
+app.get("/", function (request, response) {
+    response.end("Welcome to the home page");
+});
+
+app.get("/about", function (request, response) {
+    response.end("Whats it all about");
+});
+
+app.get("/weather", function (request, response) {
+    response.end("You always take the weather with you");
+});
+
 app.use(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain" });
-    response.end("Did you find a static file");
-})
+    response.statusCode = 404;
+    response.end("Page not found");
+});
+
 
 http.createServer(app).listen(3000);
