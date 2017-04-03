89d50d5d584dfbe362309d90f92b691375ba4d26
Bill Moore <bill@nzmoore.com>
2017-04-02 20:06:43 -0400

    Using Morgan for logging

--- Chapter3/app.js
+++ Chapter3/app.js
@@ -1,25 +1,14 @@
 var express = require("express");
 var http = require("http");
+var logger = require("morgan");
 
 var app = express();
 
-app.use(function(request,response, next) {
-  console.log("In comes a request to " + request.url);
-  next();
-});
-
-app.use(function(request,response, next) {
-  var minute = (new Date()).getMinutes();
-  if ( (minute % 2) === 0) {
-    next();
-  } else {
-    response.statusCode = 403;
-    response.end("Not authorized");
-  }
-});
+app.use(logger("short"));
 
 app.use(function (request, response) {
-    response.end("Secret info: the password is 'Swordfish!' ");
+    response.writeHead(200, {"Content-Type": "text/plain" });
+    response.end("Hi there again with Morgan logging");
 })
 
 http.createServer(app).listen(3000);
