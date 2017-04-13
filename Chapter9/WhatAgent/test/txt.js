45743fb65e3ce434a30b87fdbcf0970786f32157
Bill Moore <bill@nzmoore.com>
2017-04-13 13:14:59 -0400

    First Supertest tests for Express User Agent app

---
+++ Chapter9/WhatAgent/test/txt.js
@@ -0,0 +1,28 @@
+var app = require("../app");
+var supertest = require("supertest");
+
+describe("plain text response", function () {
+  it("returns a plain text response", function (done) {
+    supertest(app)
+      .get("/")
+      .set("User-Agent", "my cool browser")
+      .set("Accept", "text/plain")
+      .expect("Content-Type", /text\/plain/)
+      .expect(200)
+      .end(done);
+  });
+
+  it("returns your User Agent", function (done) {
+    supertest(app)
+      .get("/")
+      .set("User-Agent", "my cool browser")
+      .set("Accept", "text/plain")
+      .expect(function (res) {
+        if (res.text !== "my cool browser") {
+          throw new Error("Response does not contain User-Agent");
+        }
+      })
+      .end(done);
+  });
+
+});
