25628fffa82f06b5c51990c759207fc170c9b9dc
Bill Moore <bill@nzmoore.com>
2017-04-15 13:13:44 -0400

    Supertest of simple app with text response - test pass

--- Chapter9/WhatAgent/app.js
+++ Chapter9/WhatAgent/app.js
@@ -2,4 +2,14 @@ var express = require("express");
 
 var app = express();
 
+app.set("port", process.env.PORT || 3000);
+
+app.get("", function (req, res) {
+  res.type("text");
+  res.send(req.headers["user-agent"]);
+});
+app.listen(app.get("port"), function () {
+  console.log("App started on port " + app.get("port"));
+}),
+
 module.exports = app;
